2dce442bc4b0b9acc6e4841bdd02d01202f9a6b9
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-06-03 12:04:34 -0600

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import { Switch, Route } from "react-router-dom";
-import Navbar from './components/NavBar';
+import Navbar from "./components/NavBar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Things from "./pages/Things";
+import Ati from "./pages/Ati";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Switch>
+      <Navbar />
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/things" component={Things} />
+        <Route exact path="/ati" component={Ati} />
       </Switch>
     </>
   );
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,15 @@
-
 import React from "react";
-import { Link  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar =()=> {
-    return (
-      <div>
-          <Link to="/">Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/things'>Things</Link>
-      </div>
-    );
-}
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/things">Things</Link>
+      <Link to="/ati">Ati</Link>
+    </div>
+  );
+};
 
 export default Navbar;
