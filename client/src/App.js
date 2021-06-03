b825d828a7206fc25c51b668ba4307a84cedf70a
landon hickman <landonhickman12@gmail.com>
2021-06-03 12:01:44 -0600

    added Landon.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Navbar from './components/NavBar';
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Landon from "./pages/Landon";
 import Things from "./pages/Things";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/things" component={Things} />
+        <Route exact path='/landon' component={Landon} />
       </Switch>
     </>
   );
