f9826b2ae21b0aa174621a667f08a8209902f06a
James Yeates <jimibue@gmail.com>
2021-06-03 11:57:31 -0600

    james

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Navbar from './components/NavBar';
 import About from "./pages/About";
 import Home from "./pages/Home";
+import James from "./pages/James";
 import Things from "./pages/Things";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/things" component={Things} />
+        <Route exact path="/james" component={James} />
       </Switch>
     </>
   );
